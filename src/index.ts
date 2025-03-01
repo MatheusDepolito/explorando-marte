70a4b49445f8717628a95655e76ee45a1c9188ce
Matheus Depolito <matheussousadg@gmail.com>
2025-03-01 13:04:36 -0300

    testing implementation roverService in index

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('setup');
+import { RoverService } from './services/RoverService';
+
+const input = ['5 5', '1 2 N', 'LMLMLMLMM', '3 3 E', 'MMRMMRMRRM'];
+
+const [ plateauSize, ...roverInputs ] = input;
+const [maxX, maxY] = plateauSize.split(' ').map(Number);
+
+const roverService = new RoverService(maxX, maxY);
+
+const results = roverService.processRovers(roverInputs);
+
+console.log(results);
